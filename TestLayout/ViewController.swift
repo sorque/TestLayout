8222769fbaf93f0ae632e25aa7eba90111566216
Sorque <michvl@yandex.ru>
2023-03-05 20:14:32 +0300

    autolayout test

--- TestLayout/ViewController.swift
+++ TestLayout/ViewController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let redView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let cyanView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(redView)
+        view.addSubview(cyanView)
+        
+        NSLayoutConstraint.activate([
+            redView.widthAnchor.constraint(equalToConstant: 50),
+            redView.heightAnchor.constraint(equalToConstant: 30),
+            redView.centerYAnchor.constraint(equalTo: cyanView.centerYAnchor),
+            redView.leadingAnchor.constraint(equalTo: cyanView.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            cyanView.widthAnchor.constraint(equalToConstant: 50),
+            cyanView.heightAnchor.constraint(equalToConstant: 30),
+            cyanView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            cyanView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
+        
     }
 
 
